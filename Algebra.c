a74bf28f351bbbd6c9d71469d7857ac89f76ce8a
Utkarsh Pratham <utx1zz@gmail.com>
2025-10-27 21:28:08 +0530

    Division and algebra

---
+++ Algebra.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a=15, b=10, sum, diff, prod, div;
+    sum = a + b;
+    diff = a - b;
+    prod = a * b;
+    div = a / b;
+
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", diff);
+    printf("Product: %d\n", prod);
+    printf("Division: %d\n", div);
+
+    return 0;
+}
---
+++ Division.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main () {
+    int a=20, b=5 , div;
+    div = a / b;
+
+    printf("Division of two numbers is %d \n", div);
+    return 0;     
+}
