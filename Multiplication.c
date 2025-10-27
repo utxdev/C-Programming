33545de975a8af08a61104b29c12cef28e11e067
Utkarsh Pratham <utx1zz@gmail.com>
2025-10-27 21:12:21 +0530

    Multipilication of numbers

---
+++ Multiplication.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 128;
+    int Multiplication;
+
+    Multiplication = a * b * c;
+
+    printf("Multiplication of the given numbers is %d\n", Multiplication);
+    return 0;
+}
