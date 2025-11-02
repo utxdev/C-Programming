2f152f7bcf8a4d8875f420a0f4658cd065125b4d
Utkarsh Pratham <utx1zz@gmail.com>
2025-11-02 18:57:05 +0530

    Learnt Relational Operators

---
+++ Relational.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    a = 34;
+    b = 34;
+    c = 24;
+
+    printf("a is equal to b: %d\n", a == b);
+    printf("a is not equal to b: %d\n", a != b);
+    printf("a is greater than c: %d\n", a > c);
+    printf("b is less than c: %d\n", b < c);
+    printf("a is greater than or equal to b: %d\n", a >= b);
+    printf("b is less than or equal to c: %d\n", b <= c);
+return 0;
+    }
