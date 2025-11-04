221afed15068a5c5fcf0a2b5152f0c3669b1d089
Utkarsh Pratham <utx1zz@gmail.com>
2025-11-04 10:19:33 +0530

    Assignment Operators

---
+++ assignment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    int x = 5;
+    printf("Initial value of x: %d\n", x); 
+
+   
+    x += 3;
+    printf("After x += 3, the new value of x is: %d\n", x); 
+
+    return 0;
+}
