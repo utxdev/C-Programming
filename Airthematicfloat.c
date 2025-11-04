f57f17b1204cf197d16eb2976ca7e1883ce75f83
Utkarsh Pratham <utx1zz@gmail.com>
2025-11-04 10:28:03 +0530

    Float Arithematic Operation

---
+++ Airthematicfloat.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main () {
+    int a;
+    float b;
+    a = 34;
+    b = 45.5;
+
+printf("a+b = %f\n", a+b);
+printf("a-b = %f\n", a-b);
+printf("a*b = %f\n", a*b);
+printf("b/a = %f\n", b/a);   
+    return 0;
+
+
+}
